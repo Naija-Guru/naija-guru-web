897f8b825cdb9ad0fb8f374c24e3f8ec97a84b41
Nicholas Kajoh <kajohterna@gmail.com>
2025-07-21 00:03:26 +0200

    Update content samples for editor

--- apps/editor/constants/content.ts
+++ apps/editor/constants/content.ts
@@ -1,61 +1,140 @@
 export const CONTENT_SAMPLES = [
   {
-    title: 'Life Journey',
-    content: `Let me tell you, life is a journey with many paths. Sometimes,
-              you will walk on <b>smooth roads</b>, everything will be joyful, but other
-              times, the path may be filled with challenges. That's why they say,
-              no matter what happens, you must stay strong and never give up. As you
-              persevere, you'll find that not every day will be easy, but the
-              determination you carry in your mind will make the difficulties less
-              burdensome. Whether in rain or sunshine, you must continue your journey
-              because persistence brings better results. In the end, it doesn't matter
-              how slow or fast you go, as long as you don't remain stagnant, you will
-              reach your destination.`,
-  },
-  // {
-  //   title: 'Nigerian Cuisine',
-  //   content: `Nigerian food is exceptional! If you haven't tried jollof rice cooked
-  //             over firewood, you're missing out. The aroma will make your stomach
-  //             rumble before you even taste it. And let's not forget our delicious
-  //             pepper soup and pounded yam with egusi. Indeed! Even <b>eba with okra</b>
-  //             soup is divine! The way we prepare our food with abundant spices and
-  //             ingredients is remarkable. That's why foreigners are so eager to try
-  //             our cuisine. If you're Nigerian and unfamiliar with these dishes,
-  //             you should reconnect with your heritage.`,
-  // },
-  {
-    title: 'Lagos Traffic',
-    content: `Anyone who has driven in Lagos traffic will understand it's a real
-              challenge. You might leave home at 5am, yet arrive at work by 9am.
-              It's often inexplicable. Drivers lack patience, everyone <b>honks
-              unnecessarily</b>, bus conductors shout, and motorcycle riders drive as
-              if they're in Formula 1. Street vendors look for customers to approach.
-              Even when it rains slightly, everywhere becomes congested. The worst part
-              is that you'll face the same traffic when returning home. But as
-              Lagosians, we've developed resilience to this situation. We continue to
-              manage it with enthusiasm.`,
+    title: 'Dem don nab me',
+    content: `Madam Gold na wicked person. Any time wey any of us sight am like this, we dey pick race. Persin no go tell us bifor we go disappear. All the people wey dey here no get mama and papa. Na Madam Gold be our mama and papa. She no be better persin, but na im dey give us chow make belle soft, and na im dey give us cloth. No be better gwanj, but last-last we see food chop.
+
+Last week, Madam Gold beat shege comot for one boy body. If you take eye see di boy, na red mark cover im body like persin wey thieve for market. Wetin e do? I no dey there but them talk say Madam Gold send am go market. Na so di boy go. Wen e reach, dem no give am change. Madam Gold talk sey di boy thieve im money. This boy wey I dey look no even sabi two plus two, how dat one go thieve from Madam Gold?
+
+No be better work Madam Gold dey give us do. Na work wey she no fit ever give her own pikin do. Time dey wey she go tell us make we gather plastic for dump. Di money wey we make, na she go keep am, like sey na she do di work. If we no find plastic, she go lock us for outside, sey make we sleep for ground. E no go better for am!
+
+Everybody dey fear Madam Gold. But me? Lai-lai! Na stand my mama take use born me. We don dey plan haw we go take show am pepper, but e be like sey di woman chop winch. Awa plan no dey ever work. But dis time, I really mean am sey I go show am sey I no dey for ha wahala. I begin dey gather spit for cup. Everybody for yard spit for inside the cup. Dem no even ask me wetin I wan use am do.
+
+Madam Gold dey cook her food separate normal-normal. She talk say she no fit chop di yeye tin-dem wey we dey chop. I sabi where she dey cook ha own. Today, she go chop wetin pass am. I see as she dey cook stew. I no sabi wer she go but I dey try rush. As I dey use one eye look back, I dey use di other one put di spit for ha stew. As I start to dey turn am, na im I hear person cough for my back. I no need look back to see sey na Madam Gold.
+
+Who do me like dis?!`,
   },
   {
-    title: 'Family Dynamics',
-    content: `In Nigeria, family matters are significant. From our parents to our
-              extended family, everyone feels entitled to offer opinions on your affairs.
-              If you want to marry, the entire community will help evaluate your
-              potential spouse. If you <b>get promoted</b> at work, everyone will expect
-              financial assistance. Your aunts will ask when you plan to have children
-              if you've been married for two years. Your uncles will question why you
-              haven't built a house in the village. The expectations never end. But
-              simultaneously, these family members will support you when times are
-              difficult. They'll prioritize your concerns when you need assistance.`,
+    title: 'One day for market',
+    content: `Today na Sunday. My chicken for yard never do kukuruku but I sabi sey time don reach make I wake up dey waka go market. I get dream for night wey na so-so ojuju dey pursue me. Tufiakwa! I reject am. Papa Ifeanyi still dey sleep, dey snore with im mouth like basket. I no sabi haw I dey manage sleep wit the kind ringtone wey dey comot from im mouth every night, but once my head touch pillow like this, I don go… I don too work.
+
+Ifeanyi still dey sleep for ground. Im sister too still dey sleep. See as she spread leg. I carry leg small-small make I no match dem as I begin dey pack my market tins. As I dey pack am dey go, na so my kpangolo drop for ground. Ifeanyi begin dey turn dey shake body, but Papa Ifeanyi? For where? That one with im generator for mouth. If I like make I break plate for im ear, e no go wake. I dey happy sey I no wake di pikin-dem. If not dem go slow my waka, begin dey ask for many tins.
+
+I carry my bag start dey go market. As I komot for my dommot, I see sey Gloria don dey wash cloth for outside.
+
+“Gloria, haw nau?”
+
+“Mama Ify, I dey o. You don dey go shop?”
+
+I no sabi di kind JAMB question wey she dey ask me. Shey she no see sey I carry load? Wich oda place I go dey go for early momo like dis? I dey wonder weda na di belle wey she carry wey make am to no sabi wetin dey happen again. Dis one go be number five wey she don born. Oga Calistus dey find boy. No be small wahala.
+
+“Ehen nau. Na market I dey go.”
+
+“Okay o, go well o.”
+
+I smile for am, komot my face. Gloria, Gloria! After she don do amebo go dey table my matter for neighbors, she go kon dey shine teeth wit me like sey we be padi. Yeye dey smell! Gloria no be my wahala today. Wen time reach, she go meet ha own for road.
+
+I continue my waka go market. Sun don begin dey shine. Pikin-dem don begin dey komot to sweep ground. Shop don dey open. Okada don dey find passenger. Everybody wit im kettle don start new day. Na haw e suppose be. No time to waste time. Everybody gat find tins to do wit dia hand.
+
+As I reach market, I see sey na me first reach market. Mummy Tope, my neighbor never reach. E better like dat. She too dey do. Once she reach like dis, na so-so cho-cho-cho she go dey do. I no mind to dey follow am talk small-small, but once she enter talk like this, she no dey fit stop. I be di kind person wey no too like talk. I fit siddon from morning till night just dey tink. But with di kind work wey I dey do, if I no talk, I no go see shi-shi. Di women for dis awa market too like gra-gra. Na only two option dey. Make yu fight gidigbo to find customer or make yu siddon dey look. Last-last wen hunger don tire yu, yu go carry yor load go village go dey look.
+
+See wetin I dey talk, I don too tink forget mysef. Make I sharp-sharp arrange my market. Day don break finish.
+
+As I dey arrange my tomato and my oda tins, na so Mummy Tope come. She bring ha pikin, Tope, come market today. Dat pikin get coconut head. She no fit keep nyash for one place. Na so-so wahala she dey cause for market.
+
+“Mummy Ify, yu early today o”, na wetin Mummy Tope talk as she begin dey arrange ha tins. I just smile small, but for my mind, na blow I want give am. Wich day I no dey come shop early?
+
+“Welcome o. Haw body?”
+
+“Ah, we thank God… Tope, yu no go greet Mama Ify?”
+
+Di mumu Tope just dey look me like sey I get shit for face. As di mama yeye, na so di pikin too yeye.
+
+“No mind Tope jare! She no well. She bin get malaria. Na wai I say make she follow me.”
+
+Na so she start her cho-cho-cho. I no sabi how she no dey taya. My own be sey na market I come sell. Make she dey talk dey go.
+
+One don nak. People don dey komot from church. Market don dey full. Na dis time I dey like market. People dey waka up and down dey buy every-every. I dey like wen all dem big-big madam come market. I dey always look dia cloth. Na so-so fine-fine tins dem dey wear. I dey pray for di day wen me sef go komot for my motor, dey wear big madam cloth and shoe. Dis kind life go fit me well-well. E get one woman wey come buy onions from me. She buy plenty onions, kon say make I hold di change on top. Di tin sweet me well-well. E don tey wey persin dash me mony. I see as mummy Tope dey look me wit one kind eye. Na she sabi. Make she sef pray for better customer.
+
+E no long before Ifeanyi come jam me for market. I dey always happy wen e come, becos wen e come, I dey quick sell my market. I no sabi the cream wey di boy dey use but I no dey complain. Na my good luck charm. I go see better money when day end.
+
+As night want reach, me and Ifeanyi start dey pack our tins make we go house go rest. As we dey pack, na so olopa rush enter inside market, gbam! People kon dey scatter. Dem kon dey shout sey them dey find Tope. Tope ke? Wetin dat one don do now? Mummy Tope dey try cover am make di olokpa no see am. Na so Ifeanyi shout sey she dey hide under table. Di olokpa just push Mummy Tope go one side, drag Tope commot for under table. Dem talk sey dem dey arrest am. Sey she steal money from persin. Last-last, dem don nab am. No be today she start dey tiff. Mummy Tope start dey cry, dey beg dem sey make dem leave ha pikin. Dem no gree o. Tope just dey look. She no send. Na so dem put am inside handcuff, dey drag am go their van.
+
+Mummy Tope leave ha market, follow dem dey beg. I kon dey tell am sey sorry o. Sey God go make dem release im pikin. If yu see di bad eye wey she give me. Wetin I do? I just dey pray for am. Shey I don do bad ni? Na she sabi. Me and Ifeanyi finish to pack our tins. Today make sense, no be small!
+
+Tope don finally see di one wey pass am.`,
   },
   {
-    title: 'Nigerian Music',
-    content: `Nigerian music has gained global recognition and it's significant. From
-              Afrobeats to Amapiano, our artists represent us excellently. Wizkid,
-              Burna Boy, Davido, Tiwa Savage, Tems - all have carried our flag to
-              international platforms. The <b>beats we produce</b> now are so appealing
-              that even foreigners dance to them in clubs. Previously, people abroad
-              weren't very familiar with our music, but now, you can hear Nigerian
-              songs on radio stations worldwide. It's evident that Nigerian music
-              will continue to dominate for many years to come.`,
-  },
+    title: 'Senior Emeka',
+    content: `Monday na serious day for me. Na the start of a new week. And as di oga wey I be for school, nothing fit miss for my dressing. For Sunday night, I dey make sure sey I don polish my shoe, make e shine well-well—na Papa teach me dat one. I go kon iron my shirt, make e stand gidigba, make di lines dem sharp. My hair nko? Baba Solo go don shape am for me on Saturday. I no dey cut am for Sunday becos if I leave am till Sunday, by Monday, all di line for wer di clipper cut me go still dey show. Na after JSS2 I learn dat one. I don upgrade.
+
+Papa get one perfume wey e dey use only for special occasions. E no know but every now and den, I dey spray di perfume before I go school. Me sef gat smell nice. Di tin don dey finish small-small. E mean sey time don reach make I buy my own. I no want the man wahala.
+
+I look my watch. Ye! Time don go. I rush do my tie. I go brush my hair for road. I carry my bag begin dey go. I no even get time put something for belle. I go buy buns for tuck shop. As I dey go, I see Chike for road. He come my side, and we begin trek together.
+
+“Haw far nau? Yu watch match yesterday?” Chike ask me.
+
+“I dey o, haw body? I no watch o. I get test today so I dey try cram some things. Wetin be scores?”
+
+“O boy, na three-two o. Di match sweet no be small. You fit come my side after. Dem go show am again for evening.”
+
+I consider am. Papa no go quick come back today so I fit go.
+
+“No wahala nau, we go see.”
+
+Our school no far, and before we close eye open am, we don reach. I be class prefect. So wen I reach school, na gate I dey stand, make I for see as all di students dey enter. Anybody wey I see wit dirty uniform, or wey no wear di correct shoe or socks, na to send am go back house. I no dey take nonsense. Everybody sabi dat one. Na wai I dey always make sure sey my own uniform dey shine. Make nothing stain my white.
+
+Emmanuel na my regular customer for dis gate. Something dey always miss for im uniform. If no be sey e wear wrong sandal, e go be sey e no wear socks, abi im shirt dirty. Di guy dey always do anyhaw. But today, as e dey walk come my side, I scan am see sey everything dey intact. E shock me o.
+
+“Emma, shey na yu I dey see so?”
+
+Di boy begin dey smile. “Senior Emeka, na me o. I no wan make yu send me go house today. In fact, I don buy new shirt. Yu no see as e white?”
+
+“I dey see am o. You try no be small. Keep am up o. I no want send yu go house again.”
+
+“No wahala, Senior Emeka. I don change”, he laugh as e dey waka go.
+
+All di students wey pass, dem do well today. Abi we dey do sometin for school today wey I no know?
+
+As I dey finish my duty for gate, time for assembly kon reach. For assembly, I dey stand wit my classmates for line. Principal begin address us. As usual, na my class collect.
+
+“SS2 B!!! Una wahala don dey plenty. Di reports wey I don dey get don fill cup. I no go take all dis rubbish from una. Una wey suppose be example to di junior ones-dem. Instead, na una just dey make shame catch us. As from today, una dey on manual labor. All of una go cut di grass wey dey field. After dat one, una go clean all di toilets wey dey for dis compound. Once una do dat one finish, I go find anoda work give una”.
+
+As class prefect wey I be, I no think sey dis punishment go reach my side. Na im I raise my hand, make I ask. Principal just clear me straight. “Di punishment na for yu too, Emeka. Everybody join for dis one”. Na so everybody begin laugh. I no just understand di man. Me wey be good student. Na even me tell di man di things wey some of my classmates-dem dey do. I don mess up. I go go meet am after assembly. After e read announcements, e talk sey assembly don end, make we dey go class.
+
+My friend Femi begin dey waka wit me as I dey go class.
+
+“Haw far nau? Wai princi go do yu like dat? E no sabi sey yu be prefect?”
+
+“No mind am joor. I go go see am later. Hope sey yu dey okay.”
+
+“I dey o. Hmm… e be like say yor girlfriend dey come o. Check back!” Femi dey smile as e dey talk am. I turn back see say Amara dey waka come. She no be my girlfriend but everybody sabi sey I like am. Na why dem dey always joke sey she be my girlfriend.
+
+“Guy, she no be my girlfriend.” Femi just laugh waka leave me for der. Amara tap me for shoulder.
+
+“Emeka, haw far?” Ha voice be like pap wey dem just komot from fire—e smooth die. I dey alway shy to follow am talk.
+
+“I… I dey fine, Amara. How you… haw yu dey?” I dey sweat.
+
+“I dey. I want sey make yu no vex as people dey laugh yu for assembly ground. Yu know haw e dey go.” She dey smile at me.
+
+“Ah no, I no dey vex. No wahala.” I no sabi weda to put my hand for pocket abi to fold am for chest. Di whole thing just dey make my body do jiggy-jiggy. Me and Amara neva talk for long like dis before, na just greet I dey greet am.
+
+“Ehen, me and my friends dey go Chikky’s after school. Yu go like come?” Chikky’s na one buka wey dey town. Students dey like go there on a regular. Amara dey ask me make I follow am comot. Abi I dey dream? I no fit believe am. Na for dis kain important time my voice like to stop to dey work.
+
+“I, err, I go…”
+
+Na so I begin cough. Today of all days na di day dis cough kon come. Dis one na serious cough. Na di one wey no matter wetin yu do, e no go stop till e don do complete.
+
+“Emeka, wetin happen? Yu dey okay?” Amara begin dey tap me for back. Dis cough no stop. E remain small make I cough out my intestines. People begin gather us. Wich kain embarrassment be dis one?
+
+“Senior Emeka, wetin happen?”
+
+“Make somebody bring am water.”
+
+“Give am space, give am space.”
+
+“Who get fan o? Make we blow am breeze.”
+
+Na so people just dey talk, dey do different tins around me. Di only thing wey dey my mind na haw I go tell Amara sey yes, I go follow am go Chikky’s.`,
+  }
 ];
